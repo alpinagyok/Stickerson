fcb2ddfaf7f9a3b8f342cb09981e66b9a169203d
Aleks Kozajev <aleks.kozaev@gmail.com>
2020-07-10 14:50:26 +0300

    Initial React Setup

--- server.js
+++ server.js
@@ -48,8 +48,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get("/", (req, res) => res.send("hello"));
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

abddd9a1b4a13d530d59b2c10ec749106cf9ef8e
Aleks Kozajev <aleks.kozaev@gmail.com>
2020-07-10 14:04:11 +0300

    Initial Node Setup

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+
+const users = require("./routes/api/users");
+// const profile = require("./routes/api/profile");
+// const posts = require("./routes/api/posts");
+
+const app = express();
+
+// Body parser middleware is not needed. express has it
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// DB Config
+const db = process.env.MONGODB_URL || require("./config/keys").mongoURL;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+// we use strategies with passport (local strategy/oauth strategy/...)
+// this is JWT strategy
+require("./config/passport")(passport); // passing passport as param
+
+// Use Routes
+app.use("/api/users", users);
+// app.use("/api/profile", profile);
+// app.use("/api/posts", posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.get("/", (req, res) => res.send("hello"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ validation/is_empty.js
@@ -0,0 +1,8 @@
+// Used to make sure that Validator doesn't break
+const isEmpty = (value) =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
